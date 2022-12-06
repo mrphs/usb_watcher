1a21a0f287f60c57abb593bff1c6147e945cb49f
Nima Fatemi <mrphs@users.noreply.github.com>
2022-12-06 00:10:26 -0800

    use notify-rust to run it in the background

--- usb_watcher_mdfetcher.rs
+++ usb_watcher_mdfetcher.rs
@@ -1,7 +1,10 @@
 use std::fs::OpenOptions;
 use std::io::prelude::*;
+use std::sync::Arc;
 use std::time::SystemTime;
+use std::thread;
 use usb::{Device, Result};
+use notify_rust::Notification;
 
 // This function logs the insertion of a USB device
 fn log_device_insertion(log_file: &str, device: &Device) -> Result<()> {
@@ -30,10 +33,32 @@ fn main() -> Result<()> {
     // Set the log file location
     let log_file = "/var/log/usb_device_metadata.log";
 
-    // Log the insertion of any USB device
-    for device in Device::list()? {
-        log_device_insertion(log_file, &device)?;
-    }
+    // Set up a watcher for USB device events
+    let mut watcher = Notification::new()?;
+    watcher.watch("/dev")?;
+
+    // Create a shared Arc to the watcher, so that it can be accessed by multiple threads
+    let watcher = Arc::new(watcher);
+
+    // Spawn a thread to handle USB device events
+    let watcher_clone = watcher.clone();
+    thread::spawn(move || {
+        // Loop indefinitely, waiting for USB device events
+        loop {
+            // Get the next USB device event
+            let event = watcher_clone.recv().unwrap();
+
+            // If the event is a device insertion, log the metadata for the device
+            if event.event_mask.contains(notify_rust::EventKind::CREATE) {
+                for device in Device::list().unwrap() {
+                    log_device_insertion(log_file, &device).unwrap();
+                }
+            }
+        }
+    });
+
+    // Wait for the watcher thread to finish
+    watcher.join().unwrap();
 
     Ok(())
 }
